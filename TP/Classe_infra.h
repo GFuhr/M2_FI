ef7d3fef00bdce2b849cf974979dc542759be231
G. Fuhr <guiyom00@gmail.com>
2018-02-22 10:10:29 +0100

    TP3 Project file

--- TP/Classe_infra.h
+++ TP/Classe_infra.h
@@ -10,13 +10,12 @@ class Infrastructure {
 public:
     Infrastructure(void);
     Infrastructure(int Na, int Nc);
-        Infrastructure(CapteurAnalogique * Vhum, CapteurDigital * Vtmp, int Nhum, int Ntmp);
+    Infrastructure(CapteurAnalogique * Vhum, CapteurDigital * Vtmp, int Nhum, int Ntmp);
     Infrastructure(std::vector<CapteurAnalogique> Vhum, std::vector<CapteurDigital> Vtmp);
     Infrastructure(Infrastructure const &p);
     ~Infrastructure(void);
 
     double mean();
-    double deviation();
     void print(void);
     double recuperer_valeur_digital(int N);
     double recuperer_valeur_analogique(int N);
